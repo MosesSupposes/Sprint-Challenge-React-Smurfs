da6074e32eb52c94612ce9c52dc5004a0d983b0d
Moses Samuel <mosamuel98@gmail.com>
2019-05-10 08:55:41 -0700

    refactor: rename param in initial get request

--- village/src/App.js
+++ village/src/App.js
@@ -14,13 +14,14 @@ class App extends Component {
 
   componentDidMount() {
     axios.get('http://localhost:3333/smurfs')
-      .then(allSmurfs => {
+      .then(res => {
         this.setState({
-          smurfs: allSmurfs.data
+          smurfs: res.data
         })
       })
       .catch(console.error.bind(console))
   }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
