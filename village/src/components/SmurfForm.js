334fcc90dce6a3ac1d87aecb3323f075de875f78
Moses Samuel <mosamuel98@gmail.com>
2019-05-10 09:27:21 -0700

    feature: papa smurf can now add more blue people to his village!

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,15 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    const { name, age, height } = this.state
+    const { gatherVillage } = this.props
+  
+    axios.post(
+      'http://localhost:3333/smurfs',
+      {name, age, height}
+    )
+    .then(gatherVillage)
+    .catch(console.error.bind(console))
 
     this.setState({
       name: '',
